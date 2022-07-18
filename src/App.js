e44a75a275c3c4d8f068cbe8c4be5bc5194ba509
manfromny <ebacikoz@gmail.com>
2022-07-17 22:18:38 -0400

    inital commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import CartProvider from "./store/CartProvider";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+
+function App() {
+  const [cartVisible, setCartVisible] = useState(false);
+
+  const showCartHandler = () => {
+    setCartVisible(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartVisible(false);
+  };
+
+  return (
+    <CartProvider>
+      {cartVisible && <Cart onHideCart={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
+  );
+}
+
+export default App;
